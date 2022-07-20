4fcd35558d332a9dbf1bdc76fb57472e9c4ebd05
Md Ridwan <72821054+mdridwwan@users.noreply.github.com>
2022-07-20 14:24:26 +0600

    last  upadete

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+const {Server} = require('socket.io');
+const path = require('path');
+
+app.use(express.static('client/build'));
+
+const io = new Server(expressServer);
+
+
+app.get('*', function(req, res){
+    res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+})
+
+
+io.on('connection', function(socket){
+    console.log('New User Connected');
+
+    socket.on('disconnect', function(){
+        console.log('User Disconnected');
+    })
+})
+
+
+expressServer.listen(5000, function(){
+    console.log('Server Running Port: 5000')
+})
